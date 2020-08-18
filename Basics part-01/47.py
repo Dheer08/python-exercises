946ba5ee2ba0b4c1df1589924d8dca5f720abdcb
Dheer08 <rakshithdheerreddy08@gmail.com>
2020-08-18 20:17:03 +0530

    50

---
+++ Basics part-01/47.py
@@ -0,0 +1,4 @@
+# Write a python program to get the path and name of the file that is currently executing.
+
+import multiprocessing
+print(multiprocessing.cpu_count())
---
+++ Basics part-01/48.py
@@ -0,0 +1,6 @@
+# Write a Python program to parse a string to Float or Integer.
+
+n = "123.456"
+print(float(n))
+
+print(int(float(n)))
